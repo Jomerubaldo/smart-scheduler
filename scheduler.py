5a0905e5717b24811dc3d7efcf25b0d635941f6b
Jomer Ubaldo <Jomerubaldo@gmail.com>
2025-06-25 00:20:35 +0800

    initial commit

--- scheduler.py
+++ scheduler.py
@@ -1,1 +1,17 @@
-print("Jomer")
+exam = {
+  "name": "",
+  "date": "",
+  "time": "",
+  "room": "",
+}
+
+exam["name"] = input("Enter Name Exam: ")
+exam["date"] = input("Enter Date: ")
+exam["time"] = input("Enter Time: ")
+exam["room"] = input("Enter Room: ")
+
+exam.append({"name": exam, "date": exam, "time": exam, "room": exam})
+print("Added Success")
+
+
+# continue tomorrow
